328e5e56ff2c081a6da363126a383bbc3327cf52
Nebula Rider <bryanreyes8991@gmail.com>
2023-11-12 13:38:35 -0600

    built server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typedefs, resolvers } = require('./schemas');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+const startApolloServer = async () => {
+  await server.start();
 
-app.use(routes);
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.use('/graphql', expressMiddleware(server));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 API server running port ${PORT}`);
+      console.log(`🌍 Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+startApolloServer();
